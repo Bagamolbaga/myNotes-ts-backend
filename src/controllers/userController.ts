af8529e2651e2925e8c696442d3df09b85fb2f8d
Bagamolbaga <40855088+Bagamolbaga@users.noreply.github.com>
2021-10-01 23:33:41 +0300

    fix url for reset password

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -131,7 +131,7 @@ export const UserController = {
                                                     password has been generated for you. To reset your password, click the
                                                     following link and follow the instructions.
                                                 </p>
-                                                <a href="http://localhost:3000/user/reset-password/${tokenId}"
+                                                <a href="${process.env.NODE_ENV === 'production' ? `https://my-notes-lyvqj9v3x-bagamolbaga.vercel.app/user/reset-password/${tokenId}` : `http://localhost:3000/user/reset-password/${tokenId}`}"
                                                     style="background:#20e277;text-decoration:none !important; font-weight:500; margin-top:35px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;">Reset
                                                     Password</a>
                                             </td>
